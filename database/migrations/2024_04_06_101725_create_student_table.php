e5029ee30beb23c3a6235b4b68754d066dc25b0f
la paix <dj.lapaix.tech@gmail.com>
2024-04-06 12:26:13 +0200

    adding model and table

---
+++ database/migrations/2024_04_06_101725_create_student_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('student', function (Blueprint $table) {
+            $table->id();
+            $table -> string("studentId");
+            $table->string("firstName");
+            $table->string("lastName");
+            $table->date("dob");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('student');
+    }
+};
